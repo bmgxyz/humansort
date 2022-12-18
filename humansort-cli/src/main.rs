0b5c01c323f275f7af925e2e773dfe1059efff42
Bradley Gannon <bradley@bradleygannon.com>
2022-12-18 09:41:51 -0500

    Continue implementing CLI

--- humansort-cli/src/main.rs
+++ humansort-cli/src/main.rs
@@ -1,6 +1,11 @@
-use std::path::PathBuf;
+use std::{
+    error::Error,
+    fs::{read_to_string, write},
+    path::PathBuf,
+};
 
 use clap::{Parser, Subcommand};
+use humansort_lib::HumansortState;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -13,7 +18,7 @@ struct Cli {
 enum Commands {
     /// Reads a line-delimited list of items and creates a humansort file
     Start {
-        /// File containing a line-delimeted list of items to be sorted
+        /// File containing a line-delimited list of items to be sorted
         #[arg(value_name = "INFILE")]
         input_file: PathBuf,
         /// Name of the humansort file to be created (defaults to
@@ -26,6 +31,9 @@ enum Commands {
         /// Humansort file to be sorted
         #[arg(value_name = "INFILE")]
         hs_file: PathBuf,
+        /// Number of items to prompt the user to sort in a single iteration
+        #[arg(value_name = "NUM_ITEMS")]
+        num_items: Option<u8>,
     },
     /// Reads a humansort file and outputs a sorted list
     Output {
@@ -35,6 +43,50 @@ enum Commands {
     },
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Start {
+            input_file,
+            hs_file,
+        } => {
+            // Read input file.
+            let infile: Vec<String> = read_to_string(input_file.clone())?
+                .lines()
+                .map(|s| s.to_string())
+                .collect();
+
+            // Convert input file to humansort state.
+            let humansort: HumansortState = infile.into();
+
+            // Write the humansort state to the output file.
+            let output = serde_json::to_string_pretty(&humansort)?;
+            let output_file = match hs_file {
+                Some(o) => o,
+                // If the user didn't supply an output path, use the input
+                // file's path with .humansort appended to it.
+                None => {
+                    let mut o = input_file.into_os_string();
+                    o.push(".humansort");
+                    o.into()
+                }
+            };
+            write(output_file, output)?;
+        }
+        Commands::Sort { hs_file, num_items } => {
+            // Read and parse humansort file.
+
+            // Prompt the user for sorting information.
+            todo!()
+        }
+        Commands::Output { hs_file } => {
+            // Read and parse humansort file.
+
+            // Print all items in descending order by rating.
+            todo!()
+        }
+    };
+
+    Ok(())
 }
--- humansort-lib/src/lib.rs
+++ humansort-lib/src/lib.rs
@@ -1,7 +1,40 @@
+use std::collections::HashSet;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
 pub struct HumansortState {
-    items: Vec<HumansortItem>,
+    state: Vec<HumansortItem>,
 }
 
+impl HumansortState {
+    fn next(&self) -> Vec<&HumansortItem> {
+        todo!()
+    }
+    fn update(&mut self, sorted: &[&HumansortItem]) {
+        todo!()
+    }
+}
+
+impl From<Vec<String>> for HumansortState {
+    fn from(items: Vec<String>) -> Self {
+        let mut state = Vec::new();
+        // Ignore duplicate items.
+        let mut unique = HashSet::new();
+        for item in items.iter() {
+            if !unique.contains(item) {
+                state.push(HumansortItem {
+                    value: item.to_string(),
+                    rating: 0.,
+                });
+                unique.insert(item.clone());
+            }
+        }
+        HumansortState { state }
+    }
+}
+
+#[derive(Deserialize, Serialize)]
 struct HumansortItem {
     value: String,
     rating: f32,
