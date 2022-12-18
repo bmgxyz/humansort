48dcfc17c4cecbcef42821f44a7dca9a0880db92
Bradley Gannon <bradley@bradleygannon.com>
2022-12-17 19:08:01 -0500

    Begin CLI and add library for common code

--- humansort-cli/src/main.rs
+++ humansort-cli/src/main.rs
@@ -1,3 +1,40 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Reads a line-delimited list of items and creates a humansort file
+    Start {
+        /// File containing a line-delimeted list of items to be sorted
+        #[arg(value_name = "INFILE")]
+        input_file: PathBuf,
+        /// Name of the humansort file to be created (defaults to
+        /// <INFILE>.humansort)
+        #[arg(value_name = "OUTFILE")]
+        hs_file: Option<PathBuf>,
+    },
+    /// Read a humansort file and interactively sort it
+    Sort {
+        /// Humansort file to be sorted
+        #[arg(value_name = "INFILE")]
+        hs_file: PathBuf,
+    },
+    /// Reads a humansort file and outputs a sorted list
+    Output {
+        /// Humansort file to be printed
+        #[arg(value_name = "INFILE")]
+        hs_file: PathBuf,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
 }
---
+++ humansort-lib/src/lib.rs
@@ -0,0 +1,8 @@
+pub struct HumansortState {
+    items: Vec<HumansortItem>,
+}
+
+struct HumansortItem {
+    value: String,
+    rating: f32,
+}
